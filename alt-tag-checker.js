3b623887ddb7de84fd606f6fb04fd1f87186d42f
Leon Fayer <lfayer@users.noreply.github.com>
2018-08-13 15:04:29 -0400

    Create alt-tag-checker.js
    Alt tag checker bookmarklet initial commit

---
+++ alt-tag-checker.js
@@ -0,0 +1,7 @@
+javascript: (function() {
+    var style = document.createElement('style'),
+        styleContent = document.createTextNode('img:not([alt]){ filter: grayscale(100%); border: 3px dashed black; }');
+    style.appendChild(styleContent);
+    var caput = document.getElementsByTagName('head');
+    caput[0].appendChild(style);
+})();
